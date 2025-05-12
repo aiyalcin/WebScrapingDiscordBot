bede37d5b0ebe926119bb98980e5ccb4b1a5f2c8
aiyalcin <ai.yalcin@student.avans.nl>
2025-05-12 15:32:06 +0200

    small start on scraper file

--- WebScraper/Main.py
+++ WebScraper/Main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 import validators
 import json
+from WebScraper import Scraper
 
 load_dotenv()
 
@@ -12,6 +13,7 @@ try:
     discordBotKey = os.getenv("discordBotToken")
 except:
     print(f"Error: {Exception}")
+
 
 class Client(commands.Bot):
     async def on_ready(self):
@@ -24,21 +26,32 @@ class Client(commands.Bot):
             print(f"Could not sync commands to guild with guild id {guild.id}")
 
 
+class TrackedWebsite:
+    def __init__(self, Id, Url, CurrentPrice):
+        self.id = Id
+        self.url = Url
+        self.currentPrice = CurrentPrice
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 GUILD_ID = discord.Object(id=1371213848518070282)
 client = Client(command_prefix="/", intents=intents)
 
+
 def isValidUrl(URL):
     return validators.url(URL) and URL.startswith(("http://", "https://"))
+
 
 @client.tree.command(name="pricetrack", description="Return list of tracked gear prices", guild=GUILD_ID)
 async def pricetrack(interaction: discord.Interaction):
     await interaction.response.send_message("Test: pricetrack")
 
+
 @client.tree.command(name="addtracker", description="Add a price tracker by supplying a site URL", guild=GUILD_ID)
 async def addtracker(interaction: discord.Interaction, addtracker:str):
     if isValidUrl(addtracker):
+        Scraper.addTracker(addtracker)
         await interaction.response.send_message(f"✅ Now tracking: {addtracker}")
     else:
         await interaction.response.send_message("❌ Invalid url!")
--- WebScraper/Scraper.py
+++ WebScraper/Scraper.py
@@ -1,1 +1,8 @@
 from bs4 import BeautifulSoup
+import requests
+
+
+def addTracker(url):
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, "lxml")
+    print(html_text)
